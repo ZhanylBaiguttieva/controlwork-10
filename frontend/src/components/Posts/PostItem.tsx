d9897b486decfc7741c5a47e06c7de5b589b471e
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-02-03 15:43:33 +0600

    Added PostItem.tsx and Posts.tsx

---
+++ frontend/src/components/Posts/PostItem.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Grid,
+  IconButton,
+  Link,
+  styled
+} from '@mui/material';
+import {apiURL} from '../../constants.ts';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
+const ImageCardMedia = styled(CardMedia) ({
+  height: 0,
+  paddingTop: '56.25%',
+});
+
+interface Props {
+  id: string;
+  header: string;
+  content: string;
+  image: string | null;
+  datetime: string;
+  onDelete: React.MouseEventHandler;
+}
+
+const MessageItem: React.FC<Props> = ({id,header,content, image, datetime,onDelete}) => {
+  const cardImage = apiURL + '/' + image || undefined;
+
+  return (
+    <Grid item xs={6} sm={4} md={3}>
+      <Card sx={{height: '100%'}}>
+        <ImageCardMedia image={cardImage}/>
+        <CardHeader title={header} />
+        <CardContent>
+          <strong>
+            {content}
+          </strong>
+          Posted on: {datetime}
+        </CardContent>
+        <CardActions>
+          <IconButton component={Link} href={'/news/' + id}>
+            <ArrowForwardIcon/>
+          </IconButton>
+          <Button color="primary" onClick={onDelete}>
+            Delete
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  );
+};
+export default MessageItem;
---
+++ frontend/src/components/Posts/Posts.tsx
@@ -0,0 +1,56 @@
+'use client';
+import {Button, Grid, Link, Typography} from '@mui/material';
+import PostItem from './PostItem.tsx';
+import {useAppDispatch, useAppSelector} from '../../app/hooks.ts';
+import {selectPosts} from '../../containers/postsSlice.ts';
+import {useEffect} from 'react';
+import {deletePost, fetchPosts} from '../../containers/postsThunks.ts';
+
+
+const Posts = () => {
+
+  const dispatch = useAppDispatch();
+  const posts = useAppSelector(selectPosts);
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
+  const removePost = async(id: string) => {
+    await dispatch(deletePost(id));
+    await dispatch(fetchPosts());
+  };
+
+  return (
+    <Grid container direction="column" spacing={2}>
+      <Grid item container justifyContent="space-between" alignItems="center">
+        <Grid item>
+          <Typography variant="h4">News</Typography>
+        </Grid>
+        <Grid item>
+          <Button color="primary" component={Link} href="/new-post">
+            Add new post
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Typography variant="h4">Posts</Typography>
+      </Grid>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {posts.map(post => (
+          <PostItem
+            key={post.id}
+            id={post.id}
+            header={post.header}
+            content={post.content}
+            image={post.image}
+            datetime={post.datetime}
+            onDelete={()=> removePost(post.id)}
+          />
+        ))}
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Posts;
